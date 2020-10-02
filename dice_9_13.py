382870ffc287fff25449aae36fb56cf998367a62
Ave Nurme <nurme.ave@gmail.com>
2020-10-02 21:16:45 +0300

    Update dice_9_13.py

--- dice_9_13.py
+++ dice_9_13.py
@@ -1,4 +1,21 @@
-"""Ex 9-13. Dice."""
+"""
+Ex 9-13. Dice.
+
+EXAMPLE:
+
+Here are the 5 rolls of a 6-sided die:
+[1, 4, 3, 3, 2]
+
+Here are the 10 rolls of a 10-sided die:
+[5, 9, 1, 5, 3, 4, 1, 1, 10, 1]
+
+Here are the 15 rolls of a 20-sided die:
+[18, 11, 8, 18, 12, 6, 18, 20, 2, 18, 3, 3, 19, 4, 7]
+
+Here are the 20 rolls of a 30-sided die:
+[14, 4, 21, 26, 3, 18, 23, 21, 27, 1, 2, 26, 23, 5, 14, 11, 20, 6, 6, 11]
+
+"""
 
 
 from random import randint
@@ -36,4 +53,3 @@ print(D6.show_result())
 print(D10.show_result())
 print(D20.show_result())
 print(D30.show_result())
-
